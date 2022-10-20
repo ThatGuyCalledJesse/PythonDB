840782876c31f70a443219d1858bc1cd2e4b64c5
Jesse Swart <jesseswart08@gmail.com>
2022-10-20 20:22:30 +0200

    database.txt is for testing, not required for usage

--- database.py
+++ database.py
@@ -25,4 +25,8 @@ class DB:
         with open(filename, 'r') as f:
             self.database = eval(f.read())
 
-
+# With the database class above, make a database object and store a list of addresses in it
+# Then, use the save function to save the database to a file named "database.txt".
+db = DB()
+db.add('address', ['123 Main St', '456 Main St', '789 Main St'])
+db.save('database.txt')
